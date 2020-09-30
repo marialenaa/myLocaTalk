f9f405c74bd3a9da1d0dceb2c67c4bd767c2cbe6
marialenaa <marialena@wanadoo.fr>
2020-09-30 08:36:30 +0200

    some design changes

--- screens/chatScreen.js
+++ screens/chatScreen.js
@@ -1,31 +1,31 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, View, StyleSheet } from 'react-native';
-import { Text, Input, Button} from 'react-native-elements';
+import { ImageBackground, StyleSheet,Keyboard,TouchableWithoutFeedback  } from 'react-native';
+import {Input, Button} from 'react-native-elements';
 
-export default function ChatScreen(){
+export default function ChatScreen(props){
 
   const [messList, setMessList] = useState([])
   const [content, setContent] = useState('')
-
+  console.log(props)
     return(
-        <View style={styles.container}>
-            <ImageBackground source={require('../assets/bub.jpg')} style={styles.image}>
+      <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+            <ImageBackground style={styles.container} source={require('../assets/bub.jpg')} style={styles.image}>
                 <Input 
                 placeholderTextColor='#ea3a3a'
-                placeholder='Your message ...'
+                placeholder='Ecrire..'
                 value={content}
                 onChangeText={(value) => setContent(value)}
                 style={styles.input}
                 />
                 <Button 
-                title='Send your message'
+                title='Lancer'
                 onPress={()=> setContent('')}
                 type='outline' 
                 titleStyle={{
                   fontWeight: "bold",
                   color:'#ea3a3a',
-                  fontSize:'15px',
+                  fontSize:15,
                 }}
                 buttonStyle={{
                   borderColor: "#ea3a3a", borderWidth:1
@@ -33,9 +33,11 @@ export default function ChatScreen(){
                 />
                 <StatusBar style="auto" />
             </ImageBackground>
-        </View>
+        </TouchableWithoutFeedback>
       )
     }
+
+
   
   const styles = StyleSheet.create({
     container: {
@@ -51,7 +53,7 @@ export default function ChatScreen(){
       }, 
     input: {
       fontWeight: "bold",
-      fontSize:'15px'
+      fontSize:15
     }
   });
   
